a623e454b0d512d828d5144e370b478caf129834
kunimomo <kinishino1fuka2@outlook.jp>
2024-07-25 14:27:08 +0000

    設定ファイルの最新化

--- system/env.nix
+++ system/env.nix
@@ -1,6 +1,17 @@
 {
-  services.openssh.enable = true;
+  # SSHサービスの設定
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+  };
 
+  # ユーザー設定
+  users.users.nixos = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [ (builtins.getEnv "SSH_PUBLIC_KEY") ];
+  };
+
+  # ネットワーク設定
   networking.interfaces.ens18 = {
     ipv4.addresses = [{
       address = "192.168.0.3";
@@ -11,4 +22,3 @@
   networking.defaultGateway = "192.168.0.1";
   networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
 }
-
