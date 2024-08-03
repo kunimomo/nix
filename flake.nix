8f9317182e2ce400b335b8075384baad4af21527
kunimomo <kinishino1fuka2@outlook.jp>
2024-08-03 15:27:10 +0000

    最新化

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # flake.nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
