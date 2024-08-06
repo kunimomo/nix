12b746876e635f6f009686569531afc1b3002c0a
kunimomo <kinishino1fuka2@outlook.jp>
2024-08-06 16:18:30 +0000

    最新化

--- system/env.nix
+++ system/env.nix
@@ -10,22 +10,6 @@ in {
     passwordAuthentication = false;
   };
 
-  # tailscaleの設定
-  #environment.systemPackages = [ pkgs.tailscale ];
-  #services.tailscale.enable = true;
-  #networking.firewall = {
-    #enable = true;
-    #trustedInterfaces = [ "tailscale0" ];
-    #allowedUDPPorts = [ config.services.tailscale.port ];
-    #allowedTCPPorts = [ 22 ];
-  #};
-
-  # ユーザー設定
-  users.users.nixos = {
-    isNormalUser = true;
-    openssh.authorizedKeys.keys = [ sshPublicKey ];
-  };
-
   # ネットワーク設定
   networking.interfaces.ens18 = {
     ipv4.addresses = [{
@@ -33,7 +17,27 @@ in {
       prefixLength = 24;
     }];
   };
-  
-  networking.defaultGateway = "192.168.0.1";
-  networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+
+  networking = {
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "8.8.8.8" "8.8.4.4" ];
+    firewall = {
+      enable = true;
+      trustedInterfaces = [ "tailscale0" ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
+      allowedTCPPorts = [ 22 ];
+    };
+  };
+
+  # tailscaleの設定
+  environment.systemPackages = [ pkgs.tailscale ];
+  services.tailscale = {
+    enable = true;
+  };
+
+  # ユーザー設定
+  users.users.nixos = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [ sshPublicKey ];
+  };
 }
