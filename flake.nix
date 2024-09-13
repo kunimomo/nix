fa38e80d38a0983384f15ac87391809ca77c7cff
kunimomo <kinishino1fuka2@outlook.jp>
2024-09-13 13:50:54 +0000

    eachDefaultSystemの導入

--- flake.nix
+++ flake.nix
@@ -6,36 +6,39 @@
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          system = system;
-          modules = [
-            ./system/configuration.nix
-            home-manager.nixosModules.home-manager
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        nixosConfigurations = {
+          nixos = nixpkgs.lib.nixosSystem {
+            system = system;
+            modules = [
+              ./system/configuration.nix
+              home-manager.nixosModules.home-manager
+            ];
+          };
+        };
+
+        homeConfigurations = {
+          myHome = home-manager.lib.homeManagerConfiguration {
+            pkgs = pkgs;
+	    modules = [
+	      ./home-manager/home.nix
+	    ];
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python38Packages.mistune_2
           ];
         };
-      };
-
-      homeConfigurations = {
-        myHome = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-	  modules = [
-	    ./home-manager/home.nix
-	  ];
-        };
-      };
-
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.python38Packages.mistune_2
-        ];
-      };
-    };
+      }
+    );
 }
